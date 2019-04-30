58f70f5c1f8f5e0fdb3cc8a4a6108c97683d0f1a
srumyantsev <operate-lover@yandex.ru>
2019-04-30 23:25:25 +0300

    Refactoring. Use generalized way for FontAwesome svgs

--- tool/SvgToDrawioLibConverter/Program.cs
+++ tool/SvgToDrawioLibConverter/Program.cs
@@ -1,23 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace SvgToDrawioLibConverter
 {
     class Program
     {
-        private static HttpClient HttpClient = new HttpClient();
         private static readonly Settings Settings = new Settings();
-        private static readonly string ExtractedArchiveFolder = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-        private static string[] SvgsFolders;
-        private static string FontawesomeDrawioLibraryNamePrefix = $"{DateTime.Now.ToString("yyyy-MM-dd")}_fontawesome_";
         private static string DrawioLibraryNameExtension = ".xml";
+        private static string SvgExtension = "*.svg";
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             try
             {
@@ -28,13 +21,11 @@ namespace SvgToDrawioLibConverter
                 IConfigurationRoot configuration = builder.Build();
                 configuration.GetSection("Settings").Bind(Settings);
 
-                await DownloadAndExtractArchive();
                 GenerateDrawioLibraries();
-                ClearTemporaryArtifacts();
             } catch (Exception ex)
             {
-                Console.WriteLine($"Exception: {ex.Message}");
-                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+                Console.WriteLine($"Exception: '{ex.Message}'");
+                Console.WriteLine($"StackTrace: '{ex.StackTrace}'");
             }
             Console.WriteLine("Press any key...");
             Console.ReadLine();
@@ -43,59 +34,25 @@ namespace SvgToDrawioLibConverter
         private static void GenerateDrawioLibraries()
         {
             Console.WriteLine($"Generating drawio libraries");
-            foreach (string svgFolderPath in SvgsFolders)
+            foreach (Library libraryInput in Settings.Libs)
             {
-                string svgsFolderName = Path.GetFileName(svgFolderPath.TrimEnd(Path.DirectorySeparatorChar)); //Actually not file name, but directory name. Expected results are "regular", "solid", etc.
-                string drawioLibraryName = $"{FontawesomeDrawioLibraryNamePrefix}{svgsFolderName}";
-                string drawioLibraryFileName = $"{drawioLibraryName}{DrawioLibraryNameExtension}";
-                string drawioLibraryPath = Path.Combine(Settings.DrawioLibrariesFolder, drawioLibraryFileName);
-                Console.WriteLine($"Generating library {drawioLibraryPath}");
+                Console.WriteLine($"Generating library '{libraryInput.Title}'");
 
                 string mxLibrary = DrawioLibraryBuilder.Build(new DrawioLibraryBuilder.Model
                 {
-                    LibraryTitle = drawioLibraryName,
-                    SvgFilePaths = Directory.EnumerateFiles(svgFolderPath, "*.svg"),
-                    AddStyleElement = true
+                    LibraryTitle = libraryInput.Title,
+                    SvgFilePaths = Directory.EnumerateFiles(libraryInput.SvgsFolder, SvgExtension),
+                    AddStyleElement = libraryInput.AddStyleElement
                 });
-                
-                Console.WriteLine($"Writing library {drawioLibraryPath}");
+
+                string drawioLibraryPath = Path.Combine(Settings.OutputFolder, $"{libraryInput.Title}{DrawioLibraryNameExtension}");
+                Console.WriteLine($"Writing library '{drawioLibraryPath}'");
                 if (!Directory.Exists(Path.GetDirectoryName(drawioLibraryPath)))
                 {
                     Directory.CreateDirectory(Path.GetDirectoryName(drawioLibraryPath));
                 }
                 File.WriteAllText(drawioLibraryPath, mxLibrary);
             }
-        }
-
-        private static async Task DownloadAndExtractArchive()
-        {
-            string archiveTempFilePath = Path.GetTempFileName();
-
-            Console.WriteLine($"Downloading archive {Settings.FontAwesomeArchiveUrl} to {archiveTempFilePath}");
-            using (HttpResponseMessage response = await HttpClient.GetAsync(Settings.FontAwesomeArchiveUrl, HttpCompletionOption.ResponseHeadersRead))
-            using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
-            {
-                using (Stream streamToWriteTo = File.Open(archiveTempFilePath, FileMode.Create))
-                {
-                    await streamToReadFrom.CopyToAsync(streamToWriteTo);
-                }
-            }
-
-            Console.WriteLine($"Extracting archive {archiveTempFilePath} to {ExtractedArchiveFolder}");
-            ZipFile.ExtractToDirectory(archiveTempFilePath, ExtractedArchiveFolder);
-
-            Console.WriteLine($"Deleting temporary archive {archiveTempFilePath}");
-            File.Delete(archiveTempFilePath);
-
-            Console.WriteLine($"Finding svgs folders {ExtractedArchiveFolder}");
-            string svgsFolder = Directory.EnumerateDirectories(ExtractedArchiveFolder, Settings.SvgsFolderSearchPattern, SearchOption.AllDirectories).Single();
-            SvgsFolders = Directory.EnumerateDirectories(svgsFolder).ToArray();
-        }
-
-        private static void ClearTemporaryArtifacts()
-        {
-            Console.WriteLine($"Deleting temporary folder {ExtractedArchiveFolder}");
-            Directory.Delete(ExtractedArchiveFolder, true);
         }
     }
 }
--- tool/SvgToDrawioLibConverter/Settings.cs
+++ tool/SvgToDrawioLibConverter/Settings.cs
@@ -1,9 +1,17 @@
-﻿namespace SvgToDrawioLibConverter
+﻿using System.Collections.Generic;
+
+namespace SvgToDrawioLibConverter
 {
     public class Settings
     {
-        public string FontAwesomeArchiveUrl { get; set; }
-        public string SvgsFolderSearchPattern { get; set; }
-        public string DrawioLibrariesFolder { get; set; }
+        public List<Library> Libs { get; set; }
+        public string OutputFolder { get; set; }
+    }
+
+    public class Library
+    {
+        public string Title { get; set; }
+        public string SvgsFolder { get; set; }
+        public bool AddStyleElement { get; set; }
     }
 }
